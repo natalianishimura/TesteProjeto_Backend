fd0fc056f9d0c37d4d7d6eeccd9ce76f8165521f
natalianishimura <nataliaharumi@hotmail.com>
2019-12-16 23:06:06 -0300

    Adicionando teste HistoriaService

--- src/test/java/com/projeto/funancial/service/HistoriaServiceTest.java
+++ src/test/java/com/projeto/funancial/service/HistoriaServiceTest.java
@@ -1,24 +1,44 @@
 package com.projeto.funancial.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.Test;
 
 import com.projeto.funancial.model.Historia;
+import com.projeto.funancial.service.HistoriaService;
 
 public class HistoriaServiceTest {
-	HistoriaService historia = new HistoriaService();
-	
-	@Test
-	public void deve_retornar_historia_por_sequencia_e_nivel() {
 
+	@Test
+	public void retornar_historia_por_sequencia_e_nivel() {
+		HistoriaRepository repositorio = Mockito.mock(HistoriaRepository.class);
 		Historia historia = new Historia();
-		
+
 		historia.setSequencia(2);
 		historia.setNivel(1);
+
+		Integer historiaEsperada = historia.getSequencia().getNivel();
+
+		HistoriaService service = new HistoriaService(repositorio);
 		
 		Integer historiaSequenciaNivel = service.getHistoriaBySequenciaAndNivel(historia.getSequencia(), historia.getNivel());
 		
-		assertEquals(historiaSequenciaNivel);
+		assertEquals(historiaEsperada, historiaSequenciaNivel);
+	}
+
+	@Test
+	public void retornar_falso_por_sequencia_e_nivel_inexistente(){
+		HistoriaRepository repositorio = Mockito.mock(HistoriaRepository.class);
+		Historia historia = new Historia();
+
+		historia.setSequencia(159);
+		historia.setNivel(11000);
+
+		HistoriaService service = new HistoriaService(repositorio);
+		
+		Integer historiaSequenciaNivel = service.getHistoriaBySequenciaAndNivel(historia.getSequencia(), historia.getNivel());
+		
+		assertFalse(historiaSequenciaNivel);
 	}
 }
